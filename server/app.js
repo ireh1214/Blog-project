94f82b871decb5a2264ab4dab8b17c55285e244d
ireh1214 <hireh1214@gmail.com>
2021-07-28 17:40:29 +0900

    7.28 리덕스와 리덕스 사가, 리듀서 등으로 뼈대를 잡아주었습니다. 프론트엔드 단도 슬슬 만들기 시작했습니다.

--- server/app.js
+++ server/app.js
@@ -1,17 +1,38 @@
 import express from "express";
 import mongoose from "mongoose";
 import config from "./config";
+import hpp from "hpp";
+import helmet from "helmet";
+import cors from "cors";
+
+// Routes
+import postsRoutes from "./routes/api/post";
+import userRoutes from "./routes/api/user";
+import authRoutes from "./routes/api/auth";
+
+import morgan from "morgan";
 
 const app = express();
 const { MONGO_URI } = config;
+app.use(hpp()); // 서버의 보안을 담당해주는 라이브러리
+app.use(helmet()); //서버의 보안을 담당해주는 라이브러리
+app.use(cors({ origin: true, credentials: true })); //싱글페이지 애플리케이션에에서 쓰인다는데 알아보기
+app.use(morgan("dev")); //개발환경을 설정해서 로그를 확인할 수 있도록 하는것
+app.use(express.json()); //익스프레스에서 제이슨 형태를 해석해주세요~ 라고 하는 것인듯// 라우터는 분기점이라는 뜻인데 책갈피같은 표시를하는거임
 
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
-  .then(() => console.log("MongoDB connecting Success!!"))
+  .then(() => console.log("MongoDB connecting에도 이상 없음! 잘 됩니다."))
   .catch((e) => console.log(e));
+
+// Use routes
 app.get("/");
+app.use("/api/post", postsRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 import app from "./app";
+import config from "./config/index";
 
-app.listen("7000", () => {
-  console.log("hi");
+const { PORT } = config;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server started on Port ${PORT}. server.js에서 실행되고 있다네요!?`
+  );
 });
